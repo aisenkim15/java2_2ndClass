9ba88fe1c9032317b507ab477f0689f6220a3f66
aisen <ck253@mail.missouri.edu>
2019-07-18 00:25:21 +0900

    c

--- src/java2_IO/CopyLines.java
+++ src/java2_IO/CopyLines.java
@@ -1,4 +1,28 @@
 package java2_IO;
 
+import java.io.*;
+
 public class CopyLines {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
+
+       try {
+           inputStream = new BufferedReader(new FileReader("executeFile.txt"));
+           outputStream = new PrintWriter(new FileWriter("newFile.txt"));
+
+           String l;
+           int counter = 0;
+           while ((l = inputStream.readLine()) != null) {
+               outputStream.println(l);
+           }
+       } finally {
+           if (inputStream != null) {
+               inputStream.close();
+           }
+           if (outputStream != null) {
+               outputStream.close();
+           }
+       }
+    }
 }
