cca363f42e88e08454da427a6496539ef0bf2d5e
aisen <ck253@mail.missouri.edu>
2019-07-25 15:20:44 +0900

    from home

--- src/JDBC/PreparedStmtExample.java
+++ src/JDBC/PreparedStmtExample.java
@@ -7,21 +7,22 @@ import java.sql.ResultSet;
 
 public class PreparedStmtExample {
     static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    static final String JDBC_DB_URL = "jdbc:oracle:thin:@192.168.3.27:1521:orcl";
+    static final String JDBC_DB_URL = "jdbc:oracle:thin:@LOCALHOST:1521:ORCL";
 
-    static final String JDBC_USER = "spring";
-    static final String JDBC_PASS = "1234";
+    static final String JDBC_USER = "HR";
+    static final String JDBC_PASS = "hr";
 
     public static void main(String[] args) {
         try {
             Class.forName(JDBC_DRIVER);
             Connection connObj = DriverManager.getConnection(JDBC_DB_URL, JDBC_USER, JDBC_PASS);
-            PreparedStatement prepStatement = connObj.prepareStatement("select ename from emp where empno = ?");
-            prepStatement.setString(1, "7369");
+            PreparedStatement prepStatement = connObj.prepareStatement("select ename, job from emp where empno = ?");
+            prepStatement.setString(1, "1122");
 
             ResultSet resObj = prepStatement.executeQuery();
             while (resObj.next()) {
-                System.out.println("이름" + resObj.getString("ename"));
+                System.out.println("이름: " + resObj.getString("ename"));
+                System.out.println("직업: " + resObj.getString("job"));
             }
         } catch (Exception sqlException) {
             sqlException.printStackTrace();
