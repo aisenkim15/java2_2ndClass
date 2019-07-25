fc496f7dc542cb78f990d78c2d79e0178e6608f4
aisen <ck253@mail.missouri.edu>
2019-07-25 19:54:06 +0900

    class 7.25

---
+++ src/JDBC/SqlMapClient.java
@@ -0,0 +1,27 @@
+package JDBC;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class SqlMapClient {
+    private static SqlSession _session=null;
+
+    static{
+        try {
+            String resource = "JDBC/config/myBatisConfig.xml";
+            Reader reader = Resources.getResourceAsReader(resource);
+            SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+            _session = sqlMapper.openSession();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static SqlSession getSqlSession() {
+        return _session;
+    }
+}
