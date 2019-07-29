e03f2c2ea665cf04d3a344867aba2666c75e0f3a
aisen <ck253@mail.missouri.edu>
2019-07-29 19:24:50 +0900

    "class 7.29"

--- src/JDBC/EntryMain.java
+++ src/JDBC/EntryMain.java
@@ -1,18 +1,17 @@
 package JDBC;
 
-import com.sun.media.jfxmedia.events.PlayerEvent;
-import org.apache.ibatis.session.SqlSession;
 
-import java.util.List;
-
-public class EntryMain {
-    public static void main(String[] args) {
-        SqlSession session = SqlMapClient.getSqlSession();
-        String player = session.selectOne("test.getFirstPlayer", 7369);
-        System.out.println("Player Name: " + player);
-
-        List<Object> playerList;
-        playerList = session.selectList("test.getFirstPlayer2");
-
-    }
-}
+//
+//import java.util.List;
+//
+//public class EntryMain {
+//    public static void main(String[] args) {
+//        SqlSession session = SqlMapClient.getSqlSession();
+//        String player = session.selectOne("test.getFirstPlayer", 7369);
+//        System.out.println("Player Name: " + player);
+//
+//        List<Object> playerList;
+//        playerList = session.selectList("test.getFirstPlayer2");
+//
+//    }
+//}getFirstPlayer2
---
+++ src/Network/DataClient.java
@@ -0,0 +1,19 @@
+package Network;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class DataClient {
+    public static void main(String[] args)
+            throws Exception {
+// new Socket("서버주소", 포트번호)
+        Socket s=new Socket("192.168.3.15",9009);
+        BufferedReader input=new BufferedReader(new InputStreamReader(s.getInputStream()));
+// 서버에서 보낸 메시지를 받음
+        String res=input.readLine();
+        System.out.println(res);
+        s.close(); //소켓 닫기(연결 종료)
+        System.exit(0); //프로그램 종료
+    }
+
+}
